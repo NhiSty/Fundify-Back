a4d485a36f4fccc3e33a5040710617ef1bd8c04c
hkanoute <abdelhamidou4@gmail.com>
2023-07-07 07:06:07 +0200

    updated docker compose & bdd

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 /* eslint-disable no-console */
 const express = require('express');
+const { Sequelize } = require('sequelize');
+const cors = require('cors');
 
 const app = express();
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const mongoose = require('mongoose');
 const path = require('path');
+const errorHandler = require('./middleware/errorHandler');
 
 app.use(bodyParser.json());
+app.use(cors());
+app.use((req, res, next) => {
+  if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
+    if (!req.is('application/json')) {
+      return res.sendStatus(400);
+    }
+  }
+  next();
+});
+
+app.use(errorHandler);
 
 const userRoutes = require('./routes/user');
 
 app.use('/api/auth', userRoutes);
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-
-mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_DB}/?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-)
-  .then(() => console.log('[\x1b[32mOK\x1b[0m] MongoDB connection'))
-  .catch((error) => console.log(`[\x1b[31mERROR\x1b[0m] ${error}`));
+const sequelize = new Sequelize(process.env.PG_DATABASE_URL);
+try {
+  sequelize.authenticate().then(() => {
+    console.log('Connection has been established successfully.');
+  });
+} catch (error) {
+  console.error('Unable to connect to the database:', error);
+}
 
 app.use('/', express.static(path.join(__dirname, './public')));
 
