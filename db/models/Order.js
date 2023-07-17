b400d346b0e44a7683eafd32d096995326bd8117
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-07-17 14:53:40 +0200

    #5 : Add order model.

---
+++ db/models/Order.js
@@ -0,0 +1,52 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../index");
+
+const Order = sequelize.define("Order", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  customerName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  customerEmail: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  billingAddress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  billingCity: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  billingZip: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  deliveryAddress: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  deliveryCity: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  deliveryZip: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  cart: {
+    type: DataTypes.JSON,
+    allowNull: false,
+  },
+  totalAmount: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  }
+});
+
+module.exports = Order;
