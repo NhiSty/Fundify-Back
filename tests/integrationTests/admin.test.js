1cf9fca59d8f6f77e2dede76c1f97c1dee506e4c
Serkan Deveci <serkand27@hotmail.com>
2023-07-21 16:13:53 +0200

    Test inté pour admin suite

--- tests/integrationTests/admin.test.js
+++ tests/integrationTests/admin.test.js
@@ -1,4 +1,4 @@
-const arrayOfMerchant = [
+const arrayOfMerchants = [
   {
     id: 1,
     companyName: 'Sopra Steria',
@@ -24,6 +24,39 @@ const arrayOfMerchant = [
     approved: false,
   }];
 
+const arrayOfTransactions = [
+  {
+    id: 1,
+    amount: 0.99,
+    merchantId: 1,
+    userId: 1,
+    currency: 'EUR',
+    status: 'CANCELLED',
+  },
+  {
+    id: 5,
+    amount: 234.56,
+    merchantId: 1,
+    userId: 2,
+    currency: 'USD',
+    status: 'CONFIRMED',
+  }];
+
+const arrayOfOperations = [
+  {
+    id: 1,
+    type: 'TOTAL',
+    amount: '2345.98',
+    transactionId: 1,
+  },
+  {
+    id: 2,
+    type: 'PARTIAL',
+    amount: '2.34',
+    transactionId: 2,
+  },
+];
+
 beforeEach(() => {
   // eslint-disable-next-line no-global-assign
   fetch = jest.fn().mockImplementation((url, data, status, responseBody) => Promise.resolve({
@@ -38,7 +71,7 @@ function getMerchantById(id, statusCodeSuccess, statusCodeUnauthorized, statusMa
   if (id === undefined || id === null || id === '' || typeof (id) !== 'number') {
     return statusMalFormed;
   }
-  const array = arrayOfMerchant.filter((data) => data.id === id);
+  const array = arrayOfMerchants.filter((data) => data.id === id);
   if (array.length === 0) {
     return statusNotFound;
   }
@@ -48,8 +81,8 @@ function getMerchantById(id, statusCodeSuccess, statusCodeUnauthorized, statusMa
   return statusCodeSuccess;
 }
 
-describe('Admin - Validate', () => {
-  it('validate should return a 200 status', async () => {
+describe('Admin - Validate merchant', () => {
+  it('validate merchant should return a 200 status', async () => {
     const id = 1;
 
     const statusCode = getMerchantById(id, 200, 401, 422, 404);
@@ -70,7 +103,7 @@ describe('Admin - Validate', () => {
     expect(fetched.responseBody)
       .toEqual({ approved: true });
   });
-  it('validate should return a 401 status', async () => {
+  it('validate merchant should return a 401 status', async () => {
     const id = 2;
     const statusCode = getMerchantById(id, 200, 401, 422, 404);
 
@@ -88,7 +121,7 @@ describe('Admin - Validate', () => {
     expect(fetched.status).toEqual(401);
     expect(fetched.responseBody).toEqual({});
   });
-  it('validate should return a 422 status', async () => {
+  it('validate merchant should return a 422 status', async () => {
     const id = '2';
     const statusCode = getMerchantById(id, 200, 401, 422, 404);
 
@@ -106,7 +139,7 @@ describe('Admin - Validate', () => {
     expect(fetched.status).toEqual(422);
     expect(fetched.responseBody).toEqual({});
   });
-  it('validate should return a 404 status', async () => {
+  it('validate merchant should return a 404 status', async () => {
     const id = 3;
     const statusCode = getMerchantById(id, 200, 401, 422, 404);
 
@@ -126,8 +159,8 @@ describe('Admin - Validate', () => {
   });
 });
 
-describe('Admin - Invalidate', () => {
-  it('invalidate should return a 200 status', async () => {
+describe('Admin - Invalidate merchant', () => {
+  it('invalidate merchant should return a 200 status', async () => {
     const id = 1;
 
     const statusCode = getMerchantById(id, 200, 401, 422, 404);
@@ -145,7 +178,7 @@ describe('Admin - Invalidate', () => {
     expect(fetched.status).toEqual(200);
     expect(fetched.responseBody).toEqual({ approved: false });
   });
-  it('invalidate should return a 401 status', async () => {
+  it('invalidate merchant should return a 401 status', async () => {
     const id = 2;
     const statusCode = getMerchantById(id, 200, 401, 422, 404);
 
@@ -163,7 +196,7 @@ describe('Admin - Invalidate', () => {
     expect(fetched.status).toEqual(401);
     expect(fetched.responseBody).toEqual({});
   });
-  it('invalidate should return a 422 status', async () => {
+  it('invalidate merchant should return a 422 status', async () => {
     const id = '2';
     const statusCode = getMerchantById(id, 200, 401, 422, 404);
 
@@ -181,7 +214,7 @@ describe('Admin - Invalidate', () => {
     expect(fetched.status).toEqual(422);
     expect(fetched.responseBody).toEqual({});
   });
-  it('invalidate should return a 404 status', async () => {
+  it('invalidate merchant should return a 404 status', async () => {
     const id = 3;
     const statusCode = getMerchantById(id, 200, 401, 422, 404);
 
@@ -269,6 +302,146 @@ describe('Admin - set user Admin', () => {
     );
     expect(fetch).toHaveBeenCalled();
     expect(fetched.status).toEqual(404);
+    expect(fetched.responseBody).toEqual({});
+  });
+});
+
+describe('Admin - Update Merchant', () => {
+  it('should return a 200 status', async () => {
+    const id = 1;
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/merchant/update',
+      {
+        id,
+        companyName: 'BMW',
+      },
+      200,
+      {
+        companyName: 'BMW',
+      },
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(200);
+    expect(fetched.responseBody).toEqual({ companyName: 'BMW' });
+  });
+  it('should return a 401 status', async () => {
+    const id = 2;
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/merchant/update',
+      {
+        id,
+        companyName: 'BMW',
+      },
+      401,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(401);
+    expect(fetched.responseBody).toEqual({});
+  });
+  it('should return a 422 status', async () => {
+    const id = 1;
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/merchant/update',
+      {
+        id,
+        companyName: 1,
+      },
+      422,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(422);
+    expect(fetched.responseBody).toEqual({});
+  });
+  it('should return a 404 status', async () => {
+    const id = 3;
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/merchant/update',
+      {
+        id,
+        companyName: 'BMW',
+      },
+      404,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(404);
+    expect(fetched.responseBody).toEqual({});
+  });
+});
+
+describe('Admin - get all merchants', () => {
+  it('should return a 200 status', async () => {
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/merchants',
+      {},
+      200,
+      arrayOfMerchants,
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(200);
+    expect(fetched.responseBody).toEqual(arrayOfMerchants);
+  });
+  it('should return a 401 status', async () => {
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/merchants',
+      {},
+      401,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(401);
+    expect(fetched.responseBody).toEqual({});
+  });
+});
+
+describe('Admin - get all transactions', () => {
+  it('should return a 200 status', async () => {
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/transactions',
+      {},
+      200,
+      arrayOfTransactions,
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(200);
+    expect(fetched.responseBody).toEqual(arrayOfTransactions);
+  });
+  it('should return a 401 status', async () => {
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/transactions',
+      {},
+      401,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(401);
+    expect(fetched.responseBody).toEqual({});
+  });
+});
+
+describe('Admin - get all operation', () => {
+  it('should return a 200 status', async () => {
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/operations',
+      {},
+      200,
+      { arrayOfOperations },
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(200);
+    expect(fetched.responseBody).toEqual({ arrayOfOperations });
+  });
+  it('should return a 401 status', async () => {
+    const fetched = await fetch(
+      'http://localhost:1337/api/admin/operations',
+      {},
+      401,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(401);
     expect(fetched.responseBody).toEqual({});
   });
 });
