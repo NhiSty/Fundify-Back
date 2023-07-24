c4baa2b2ff74a1229d238168e1f0b83572f2e6fc
hkanoute <abdelhamidou4@gmail.com>
2023-07-24 14:55:46 +0200

    updated cookie fetching from midlware

--- middleware/transaction.js
+++ middleware/transaction.js
@@ -2,7 +2,7 @@ const jsonwebtoken = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
   try {
-    const token = req.headers.cookie.split('=')[1];
+    const token = req.headers.cookie.split('token=')[1];
     const decodedToken = jsonwebtoken.verify(token, `${process.env.JWT_SECRET}`);
     const { id, approved } = decodedToken;
 
