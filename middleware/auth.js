046a1e6dc82f7e400cb328d6386d6f60add1cb93
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-07-24 15:06:30 +0200

    Save progress.

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jsonwebtoken = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.cookie.split('=')[1];
+    const token = req.headers.cookie.split('token=')[1];
     const decodedToken = jsonwebtoken.verify(token, `${process.env.JWT_SECRET}`);
     const { id } = decodedToken;
     if (!id) {
