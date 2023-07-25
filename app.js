90365dc540e414290630bfd1e7ba43722248fc40
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-07-25 17:43:28 +0200

    Save progress.

--- app.js
+++ app.js
@@ -18,7 +18,12 @@ const adminRoutes = require('./routes/admin');
 app.use(bodyParser.json({ limit: '5mb' }));
 app.use(cors(
   {
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:8080',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://localhost:3000',
+    ],
     credentials: true,
   },
 ));
