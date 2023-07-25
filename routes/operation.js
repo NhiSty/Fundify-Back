094a4144104777b2f566b4f1d34f579b6ad7d560
Thomas J <thomas.jallu@gmail.com>
2023-07-25 10:00:09 +0200

    Mini fix

--- routes/operation.js
+++ routes/operation.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const operationMiddleware = require('../middleware/operation');
 
 const router = express.Router();
-router.use(operationMiddleware);
 
 const operationCtrl = require('../controllers/operation');
 
