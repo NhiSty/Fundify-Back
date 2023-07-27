3ccccd5b9b9a908b7591f3c05aad7e43ff2e6b2e
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-07-27 20:13:08 +0200

    Update CORS.

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(cors(
       'http://localhost:5173',
       'http://127.0.0.1:5173',
       'http://localhost:3000',
+      'https://backend.fundify.shop',
     ],
     credentials: true,
   },
