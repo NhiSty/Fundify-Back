54970d9f835bf5ac01c3ce0489e2c34293f512df
Nicolas Barbarisi <nico.bar2012@gmail.com>
2023-07-28 15:30:38 +0200

    Fix.

--- migrate.js
+++ migrate.js
@@ -4,9 +4,22 @@ try {
   db.connection.authenticate().then(() => {
     // eslint-disable-next-line no-console
     console.log('Connection has been established successfully.');
-    db.connection.sync({ force: true }).then(() => {
+    db.connection.sync({ force: true }).then(async () => {
       // eslint-disable-next-line no-console
       console.log('Database synchronized');
+
+      // Define the user data you want to add during migration
+      const userAdmin = {
+        username: 'admin',
+        email: 'admin@gmail.com',
+        password: 'Test1234!',
+        isAdmin: true,
+        // Add other attributes as needed
+      };
+
+      // Create a new user in the database
+      const newUser = await db.User.create(userAdmin);
+      console.log('User added:', newUser.toJSON());
     });
   });
 } catch (error) {
