fff8a39eee8003e5bc35fb1b4519b8edf899cb32
ThomasDev <61602867+ThomasDev6@users.noreply.github.com>
2023-07-30 17:16:54 +0200

    Test api sur opération

--- tests/unitTests/operation.test.js
+++ tests/unitTests/operation.test.js
@@ -1,140 +1,222 @@
-const OperationValidator = require('../../validator/OperationValidator');
-const TransactionMDb = require('../../mongoDb/models/Transaction');
+const transactions = [
+  {
+    id: 'uuidTransaction1',
+    amount: 0.99,
+    merchantId: 'uuid1',
+    userId: 'userId1',
+    currency: 'EUR',
+    status: 'created',
+  },
+  {
+    id: 'uuidTransaction4',
+    amount: 234.56,
+    merchantId: 'uuid1',
+    userId: 'userId1',
+    currency: 'USD',
+    status: 'captured',
+  }];
 
-describe('Operation type validator', () => {
-  it('valid type should return true', () => {
-    expect(OperationValidator.validateType('capture')).toBe(true);
-    expect(OperationValidator.validateType('refund')).toBe(true);
-  });
-  it('empty type should return false', () => {
-    expect(OperationValidator.validateType('')).toBe(false);
-  });
-  it('null type should return false', () => {
-    expect(OperationValidator.validateType(null)).toBe(false);
-  });
-  it('undefined type should return false', () => {
-    expect(OperationValidator.validateType(undefined)).toBe(false);
-  });
-  it('incorrect type should return false', () => {
-    expect(OperationValidator.validateType('NOTEXIST')).toBe(false);
-  });
+const operations = [
+  {
+    id: 'uuidOperation1',
+    type: 'capture',
+    amount: 2345.98,
+    transactionId: 'uuidTransaction1',
+    status: 'created',
+  },
+];
+
+const action = {
+  operations,
+  transactions,
+};
+beforeEach(() => {
+  // eslint-disable-next-line no-global-assign
+  fetch = jest.fn().mockImplementation((url, data, status, responseBody) => Promise.resolve({
+    // url : correspond à l'url de la requête
+    url,
+    // data : correspond au body de la requête
+    data,
+    // status : correspond au status de la response
+    status,
+    // responseBody : correspond au body de la response
+    responseBody,
+  }));
 });
 
-jest.mock('../../mongoDb/models/Transaction', () => ({
-  findOne: jest.fn(() => ({
-    refundAmountAvailable: 100,
-  })),
-}));
-describe('Operation amount validator', () => {
-  beforeEach(() => {
-    TransactionMDb.findOne.mockResolvedValueOnce({
-      transactionId: 'uuid',
-      refundAmountAvailable: 100,
+function getStatusCode(
+  needParams,
+  actionName,
+  id,
+  role,
+  statusCodeSuccess,
+  statusCodeUnauthorized,
+  statusForbidden,
+  statusMalFormed,
+  statusNotFound,
+  isLogged,
+) {
+  if (needParams === true) {
+    if (id === undefined || id === null || id === '' || typeof (id) !== 'string') {
+      return statusMalFormed;
+    }
+    const array = actionName.filter((data) => data.id === id);
+    if (array.length === 0) {
+      return statusNotFound;
+    }
+    if (!isLogged) {
+      return statusCodeUnauthorized;
+    }
+    if (role !== 'admin') {
+      return statusForbidden;
+    }
+    return statusCodeSuccess;
+  }
+  if (!isLogged) {
+    return statusCodeUnauthorized;
+  }
+  if (role !== 'admin') {
+    return statusForbidden;
+  }
+  return statusCodeSuccess;
+}
+
+describe('Operation - post create operation', () => {
+  it('should return a 201 status', async () => {
+    const transactionId = 'uuidTransaction1';
+    const statusCode = getStatusCode(true, action.transactions, transactionId, 'admin', 201, 401, 403, 422, 404, true);
+    const fetched = await fetch(
+      'http://localhost:1337/api/operations',
+      {
+        transactionId,
+        amount: 0.99,
+      },
+      statusCode,
+      {
+        id: 'uuid111',
+        transactionId: 2,
+        amount: 2345.98,
+      },
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(201);
+    expect(fetched.responseBody).toEqual({
+      id: 'uuid111',
+      transactionId: 2,
+      amount: 2345.98,
     });
   });
-
-  afterEach(() => {
-    jest.clearAllMocks();
+  it('should return a 401 status', async () => {
+    const transactionId = 'uuidTransaction1';
+    const statusCode = getStatusCode(true, action.transactions, transactionId, 'admin', 201, 401, 403, 422, 404, false);
+    const fetched = await fetch(
+      'http://localhost:1337/api/operations',
+      {
+        transactionId,
+        amount: 0.99,
+      },
+      statusCode,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(401);
+    expect(fetched.responseBody).toEqual({});
   });
-
-  it('valid amount should return true', async () => {
-    const amount = 50;
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(true);
+  it('should return a 422 status', async () => {
+    const transactionId = 1;
+    const statusCode = getStatusCode(true, action.transactions, transactionId, 'admin', 201, 401, 403, 422, 404, true);
+    const fetched = await fetch(
+      'http://localhost:1337/api/operation/create',
+      {
+        transactionId,
+        amount: 0.99,
+      },
+      statusCode,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(422);
+    expect(fetched.responseBody).toEqual({});
   });
-
-  it('valid amount with coma should return true', async () => {
-    const amount = 50.50;
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(true);
-  });
-
-  it('valid amount with comma and starting with 0 should return true', async () => {
-    const amount = 0.50;
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(true);
-  });
-
-  it('refundAmountAvailable lower than amount should return true', async () => {
-    const amount = 50;
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(true);
-  });
-
-  it('refundAmountAvailable greater than amount should return false', async () => {
-    const amount = 200;
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(false);
-  });
-
-  it('not valid amount (string number) should return false', async () => {
-    const amount = '0.50';
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(false);
-  });
-
-  it('not valid amount (string) should return false', async () => {
-    const amount = 'invalide';
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(false);
-  });
-
-  it('null amount should return false', async () => {
-    const amount = null;
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(false);
-  });
-
-  it('null amount should return false', async () => {
-    const amount = undefined;
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(false);
-  });
-
-  it('not valid amount with lot of digit should return false', async () => {
-    const amount = 0.123456789;
-    const transactionId = 'uuid';
-
-    const result = await OperationValidator.validateAmount(amount, transactionId);
-    expect(result).toBe(false);
+  it('should return a 404 status', async () => {
+    const transactionId = 'uuid9';
+    const statusCode = getStatusCode(true, action.transactions, transactionId, 'admin', 201, 401, 403, 422, 404, true);
+    const fetched = await fetch(
+      'http://localhost:1337/api/operation/create',
+      {
+        transactionId,
+        amount: 0.99,
+        type: 'PARTIAL',
+      },
+      statusCode,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(404);
+    expect(fetched.responseBody).toEqual({});
   });
 });
 
-describe('Operation status validator', () => {
-  it('valid type should return true', () => {
-    expect(OperationValidator.validateStatus('created')).toBe(true);
-    expect(OperationValidator.validateStatus('processing')).toBe(true);
-    expect(OperationValidator.validateStatus('done')).toBe(true);
-    expect(OperationValidator.validateStatus('failed')).toBe(true);
+describe('Operation - get operation by id', () => {
+  it('should return a 200 status', async () => {
+    const operationId = 'uuidOperation1';
+    const statusCode = getStatusCode(true, action.operations, operationId, 'admin', 200, 401, 403, 422, 404, true);
+    const fetched = await fetch(
+      `http://localhost:1337/api/operations/${operationId}`,
+      {},
+      statusCode,
+      // correspond au body de la response
+      {
+        id: 'uuidOperation1',
+        transactionId: 1,
+        amount: 0.99,
+      },
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(200);
+    expect(fetched.responseBody).toEqual({
+      id: 'uuidOperation1',
+      transactionId: 1,
+      amount: 0.99,
+    });
   });
-  it('empty type should return false', () => {
-    expect(OperationValidator.validateStatus('')).toBe(false);
+  it('should return a 401 status', async () => {
+    const operationId = 'uuidOperation1';
+    const statusCode = getStatusCode(true, action.operations, operationId, 'admin', 200, 401, 403, 422, 404, false);
+    const fetched = await fetch(
+      `http://localhost:1337/api/operations/${operationId}`,
+      {},
+      statusCode,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(401);
+    expect(fetched.responseBody).toEqual({});
   });
-  it('null type should return false', () => {
-    expect(OperationValidator.validateStatus(null)).toBe(false);
+  it('should return a 422 status', async () => {
+    const operationId = 1;
+    const statusCode = getStatusCode(true, action.operations, operationId, 'admin', 200, 401, 403, 422, 404, true);
+    const fetched = await fetch(
+      `http://localhost:1337/api/operations/${operationId}`,
+      {},
+      statusCode,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(422);
+    expect(fetched.responseBody).toEqual({});
   });
-  it('undefined type should return false', () => {
-    expect(OperationValidator.validateStatus(undefined)).toBe(false);
-  });
-  it('incorrect type should return false', () => {
-    expect(OperationValidator.validateStatus('NOTEXIST')).toBe(false);
+  it('should return a 404 status', async () => {
+    const operationId = 'uuid9';
+    const statusCode = getStatusCode(true, action.operations, operationId, 'admin', 200, 401, 403, 422, 404, true);
+    const fetched = await fetch(
+      `http://localhost:1337/api/operation/${operationId}`,
+      {},
+      statusCode,
+      {},
+    );
+    expect(fetch).toHaveBeenCalled();
+    expect(fetched.status).toEqual(404);
+    expect(fetched.responseBody).toEqual({});
   });
 });
